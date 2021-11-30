b3fd74915cbf414096f0d281aa7842d19ea7f4a0
cristianblar <cristian@cristianblar.dev>
2021-11-29 21:34:58 -0500

    style: Add footer styles

--- components/Footer.js
+++ components/Footer.js
@@ -4,44 +4,43 @@ import {
   AiOutlineLinkedin,
   AiOutlineProfile
 } from 'react-icons/ai'
+import styles from '../styles/Footer.module.css';
 
 export default function Footer() {
   return (
-    <div>
-      <div>
-        <a
-          aria-label='Send email to the author'
-          href='mailto:cristianblar12@icloud.com'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <AiOutlineMail title='email' />
-        </a>
-        <a
-          aria-label='GitHub Profile'
-          rel='noreferrer external'
-          href='https://github.com/cristianblar'
-          target='_blank'
-        >
-          <AiOutlineGithub title='GitHub' />
-        </a>
-        <a
-          aria-label='LinkedIn profile'
-          rel='noreferrer external'
-          href='https://www.linkedin.com/in/cristianblandon/'
-          target='_blank'
-        >
-          <AiOutlineLinkedin title='LinkedIn' />
-        </a>
-        <a
-          aria-label='Portfolio'
-          rel='noreferrer'
-          href='https://www.cristianblar.dev'
-          target='_blank'
-        >
-          <AiOutlineProfile title='Portfolio' />
-        </a>
-      </div>
+    <div className={styles.footer__container}>
+      <a
+        aria-label='Send email to the author'
+        href='mailto:cristianblar12@icloud.com'
+        target='_blank'
+        rel='noreferrer'
+      >
+        <AiOutlineMail title='email' />
+      </a>
+      <a
+        aria-label='GitHub Profile'
+        rel='noreferrer external'
+        href='https://github.com/cristianblar'
+        target='_blank'
+      >
+        <AiOutlineGithub title='GitHub' />
+      </a>
+      <a
+        aria-label='LinkedIn profile'
+        rel='noreferrer external'
+        href='https://www.linkedin.com/in/cristianblandon/'
+        target='_blank'
+      >
+        <AiOutlineLinkedin title='LinkedIn' />
+      </a>
+      <a
+        aria-label='Portfolio'
+        rel='noreferrer'
+        href='https://www.cristianblar.dev'
+        target='_blank'
+      >
+        <AiOutlineProfile title='Portfolio' />
+      </a>
     </div>
   )
 }
