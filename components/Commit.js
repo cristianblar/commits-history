1aa51acf5310f5c5a27f3707b8328fe6f0ef5e18
cristianblar <cristian@cristianblar.dev>
2021-11-29 21:18:53 -0500

    style: Add all the styles for Commit component

--- components/Commit.js
+++ components/Commit.js
@@ -1,3 +1,5 @@
+import styles from '../styles/Commit.module.css'
+
 export default function Commit({
   sha,
   authorName,
@@ -6,14 +8,22 @@ export default function Commit({
   message,
   commitUrl
 }) {
+  const parsedDate = new Date(date)
+
   return (
-    <article>
-      <div>
-        <h3>{sha}</h3>
-        <span>{date}</span>
-      </div>
-      <h5>{`${authorName} (${authorEmail})`}</h5>
-      <p>{message}</p>
-    </article>
+    <a href={commitUrl} rel='noreferrer' target='_blank'>
+      <article className={styles.commit__container}>
+        <div className={styles.container__title_container}>
+          <h3>{sha}</h3>
+          <span>{`${parsedDate.getDay()}/${parsedDate.getMonth()}/${parsedDate.getFullYear()} (${parsedDate.getHours()}:${
+            parsedDate.getMinutes().toString().length === 1
+              ? '0' + parsedDate.getMinutes()
+              : parsedDate.getMinutes()
+          })`}</span>
+        </div>
+        <h5>{`${authorName} (${authorEmail})`}</h5>
+        <p>{message}</p>
+      </article>
+    </a>
   )
 }
